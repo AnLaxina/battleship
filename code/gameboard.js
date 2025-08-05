4ceaa5bd43edb51073951489c9c98c914b6dad1c
AnLaxina <anilovlaxina@gmail.com>
2025-08-05 09:22:48 -0700

    feat(gameboard): add new method for board creation

--- code/gameboard.js
+++ code/gameboard.js
@@ -1,5 +1,19 @@
 export default class Gameboard {
-    constructor(size = 10) {
-        this.size = size;
+  constructor(size = 10) {
+    this.size = size;
+    this.board = this.#createBoard();
+  }
+
+  #createBoard() {
+    const boardToReturn = [];
+    for (let row = 1; row <= this.size; row++) {
+      for (let col = 1; col <= this.size; col++) {
+        const coordinates = [];
+        coordinates.append(row);
+        coordinates.append(col);
+        boardToReturn.apppend(coordinates);
+      }
     }
+    return boardToReturn;
+  }
 }
