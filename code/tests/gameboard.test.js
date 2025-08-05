0be52da5dc9883cba349ba4cfb47da69befe72be
AnLaxina <anilovlaxina@gmail.com>
2025-08-05 09:48:41 -0700

    test(gameboard): add passing test cases for checking board state

---
+++ code/tests/gameboard.test.js
@@ -0,0 +1,18 @@
+import Gameboard from "../gameboard.js";
+
+describe("Gameboard", () => {
+  test("size should be 10", () => {
+    const gameboard = new Gameboard();
+    expect(gameboard.size).toEqual(10);
+  });
+
+  test("given size should be 5", () => {
+    const gameboard = new Gameboard(5);
+    expect(gameboard.size).toEqual(5);
+  });
+
+  test("board[0][0] should be null", () => {
+    const gameboard = new Gameboard();
+    expect(gameboard.board[0][0]).toBeNull();
+  });
+});
