d5081636b55b3647b7570f78c49c9dc9c143fe5a
AnLaxina <anilovlaxina@gmail.com>
2025-08-06 16:44:02 -0700

    test(player): add test cases for `attack`
    Did not include test cases for `Player`'s `placeShip` mehod as that is just an abstraction from `Gameboard` which has already been tested.

---
+++ code/tests/player.test.js
@@ -0,0 +1,17 @@
+import Player from "../player.js";
+
+describe("Player", () => {
+  test("invalid attack should be null", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+
+    expect(player1.attack(player2, [11, 11])).toBe(null);
+  });
+
+  test("valid attack returns attack coordinate", () => {
+    const player1 = new Player();
+    const player2 = new Player();
+
+    expect(player1.attack(player2, [0, 0])).toEqual([0, 0]);
+  });
+});
