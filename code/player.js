1354ffe058f07cbabb6b7d0939899afb908c6bab
AnLaxina <anilovlaxina@gmail.com>
2025-08-06 16:36:19 -0700

    feat(player): add method for attacking an opponent

--- code/player.js
+++ code/player.js
@@ -5,4 +5,12 @@ export default class Player {
     this.type = type;
     this.gameboard = new Gameboard();
   }
+
+  attack(opponent, coordinates) {
+    return opponent.gameboard.receiveAttack(coordinates);
+  }
+
+  placeShip(ship, startCoord, isVertical = false) {
+    this.gameboard.placeShip(ship, startCoord, isVertical);
+  }
 }
