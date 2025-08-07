0de4841c304d3eb9ca19c5d52a1405e59145ca04
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-08-07 10:56:38 -0700

    refactor(initializeBoard): change method name to make it more clear

--- code/js/DOM/gameboardDOM.js
+++ code/js/DOM/gameboardDOM.js
@@ -1,7 +1,7 @@
 export default class GameboardDOM {
   static BOARD_SIZE = 10;
 
-  static populateBoard(playerId) {
+  static initializeBoard(playerId) {
     const gameboardDiv = document.querySelector(`#${playerId}`);
     for (let i = 0; i < this.BOARD_SIZE; i++) {
       for (let r = 0; r < this.BOARD_SIZE; r++) {
--- main.js
+++ main.js
@@ -4,5 +4,5 @@ import Player from "./code/js/player.js";
 
 console.log("This is the start of Battleship!");
 
-GameboardDOM.populateBoard("player1-board");
-GameboardDOM.populateBoard("player2-board");
+GameboardDOM.initializeBoard("player1-board");
+GameboardDOM.initializeBoard("player2-board");
