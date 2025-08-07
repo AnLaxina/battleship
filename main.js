5bf9e9a570916f0d72fa2f2b3ac10cc56d07485c
Anilov Laxina <78817046+AnLaxina@users.noreply.github.com>
2025-08-07 10:26:09 -0700

    feat(main.js): add testing methods for populating board

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+import GameboardDOM from "./code/js/DOM/gameboardDOM.js";
+
+import Player from "./code/js/player.js";
+
 console.log("This is the start of Battleship!");
+
+GameboardDOM.populateBoard("player1-board");
+GameboardDOM.populateBoard("player2-board");
