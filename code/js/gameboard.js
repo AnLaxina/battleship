45506f370da1bcf6b20c16b2c442fd8dcc3abf91
AnLaxina <anilovlaxina@gmail.com>
2025-08-10 21:36:18 -0700

    feat(gameboard): change logic for `receiveAttack`

--- code/js/gameboard.js
+++ code/js/gameboard.js
@@ -7,7 +7,7 @@ export default class Gameboard {
     this.missedAttacks = 0;
     // Here, shipId: {ship, coordinates:Set, hits:Set}
     this.ships = new Map();
-    // "x, y" that were attacked
+    // "x,y" that were attacked
     this.fired = new Set();
   }
 
@@ -15,27 +15,48 @@ export default class Gameboard {
     const [x, y] = pairOfCoordinates;
 
     if (this.#isOutOfBounds(x) || this.#isOutOfBounds(y)) {
-      return null;
+      return { ok: false, reason: "out of bounds" };
     }
 
-    const currentPosition = this.board[x][y];
+    const key = `${x},${y}`;
 
+    if (this.fired.has(key)) {
+      return { ok: false, reason: "already fired!" };
+    }
+
+    this.fired.add(key);
+
+    const cell = this.board[x][y];
     // To denote that there is a ship on the board, each "cell" will have a value of the specific ship object
     // if the cell is empty, then the cell would just be null
-    if (currentPosition !== null) {
-      currentPosition.hit();
-    } else {
+    if (cell === null) {
       this.missedAttacks++;
+      return { ok: true, hit: false, coordinates: [x, y] };
     }
 
-    return pairOfCoordinates;
+    // If there's a hit
+    cell.hit = true;
+    const info = this.ships.get(cell.shipId);
+    info.hits.add(key);
+    info.ship.hit();
+
+    const sunk = info.ship.isSunk();
+
+    return {
+      ok: true,
+      hit: true,
+      coordinates: [x, y],
+      shipId: cell.shipId,
+      sunk,
+      sunkCoords: sunk
+        ? Array.from(info.coordinates).map((s) => s.split(",").map(Number))
+        : undefined,
+    };
   }
 
   allShipsSunk() {
-    for (const ship of this.ships) {
-      if (!ship.isSunk()) {
-        return false;
-      }
+    for (const { ship } of this.ships.values()) {
+      if (!ship.isSunk()) return false;
     }
     return true;
   }
